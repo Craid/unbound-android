bd0985150753bb018971b83a89df338c72f6561b
Craid <michel.schmitz_1992@hotmail.com>
2015-11-19 10:46:55 +0100

    Deleted test comment

--- src/de/thm/sag/unbound/android/AndroidLauncher.java
+++ src/de/thm/sag/unbound/android/AndroidLauncher.java
@@ -7,11 +7,12 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import de.thm.sag.unbound.UnboundGame;
 
 public class AndroidLauncher extends AndroidApplication {
+	
 	@Override
-	//Test
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new UnboundGame(), config);
 	}
+	
 }
