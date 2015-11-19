e053401bbe8243633b5b3020f8db06cf86a5ccff
Craid <michel.schmitz_1992@hotmail.com>
2015-11-19 01:33:41 +0100

    Initial

---
+++ src/de/thm/sag/unbound/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.thm.sag.unbound.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.thm.sag.unbound.UnboundGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new UnboundGame(), config);
+	}
+}
