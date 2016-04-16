d1e0e38cae8a9fa6901cfee0b8f7c9737b286197
Craid <michel.schmitz_1992@hotmail.com>
2016-04-16 04:48:49 +0200

    Updated start game to TestGame

--- src/de/thm/sag/unbound/android/AndroidLauncher.java
+++ src/de/thm/sag/unbound/android/AndroidLauncher.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
-import de.unbound.UnboundGame;
+import de.unbound.TestGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	
@@ -13,7 +13,7 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new UnboundGame(), config);
+		initialize(new TestGame(), config);
 	}
 	
 }
